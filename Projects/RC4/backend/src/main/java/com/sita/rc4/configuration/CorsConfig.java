f26ae05b96942c08c818ba6b1eed2384fef92055
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2025-02-08 14:37:44 -0500

    Fix CORS and authentication issues

--- Projects/RC4/backend/src/main/java/com/sita/rc4/configuration/CorsConfig.java
+++ Projects/RC4/backend/src/main/java/com/sita/rc4/configuration/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.sita.rc4.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all endpoints
-                        .allowedOrigins("http://localhost:3000") // Allow requests from this origin
+                        .allowedOrigins("https://react-frontend-29tk.onrender.com/") // Allow requests from this origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow cookies and credentials
